0063ddef67688ca931f8b6a24e50fdac4c3d2b7c
Davi Monteiro <davi321522@gmail.com>
2024-07-05 15:44:57 -0300

    att00

---
+++ Models/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Catch_C_.Models
+{
+    internal class Calculadora
+    {
+        public void Dividir(int a, int b)
+        {
+            int resultado = a / b;
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Try_Catch_C_.Models;
+
+Calculadora calculadora = new Calculadora();
+
+//corrija seguindo o exemplo do gpt
+try
+{
+    //coloque a variável do resultado aqui:
+
+    calculadora.Dividir(10, 0);
+}
+catch
+{
+    //coloque o tratamento de exceção aqui:
+
+    return;
+}
